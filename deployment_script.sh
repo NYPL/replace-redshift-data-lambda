6da84a9c5b7f14acb2d058fe08e251487c5a238c
Aaron Friedman <aaronfriedman6@gmail.com>
2023-02-14 16:48:16 -0500

    Use nypl-py-utils for generic functionality

--- deployment_script.sh
+++ deployment_script.sh
@@ -5,6 +5,3 @@ cd package
 zip -r ../deployment-package.zip .
 cd ..
 zip deployment-package.zip lambda_function.py
-zip deployment-package.zip helpers
-zip deployment-package.zip helpers/kms_helper.py
-zip deployment-package.zip helpers/log_helper.py
